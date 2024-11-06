b564a8f754c2713aca072bd147c0d37c784c1f30
Agnes Iradukunda <kundaaggy@gmail.com>
2024-11-06 20:20:00 +0200

    8 kyu Grasshopper - Array Mean

---
+++ 8 kyu/8 kyu Grasshopper - Array Mean.js
@@ -0,0 +1,12 @@
+/**
+Find Mean
+Find the mean (average) of a list of numbers in an array.
+
+Information
+To find the mean (average) of a set of numbers add all of the numbers together and divide by the number of values in the list.
+
+For an example list of 1, 3, 5, 7
+
+1. Add all of the numbers
+ */
+const findAverage = (nums) => nums.reduce((acc, curr) => acc + curr) / nums.length
